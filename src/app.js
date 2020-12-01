d9b171d4e6c072e2f431209b703db2b43dfd2369
Mandy Alber <mandy.alber@gmail.com>
2020-12-01 12:12:51 -0800

    updated cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN, NODE_ENV } = require('./config')
+
 
 const app = express()
 
@@ -13,11 +14,13 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}))
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,9 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET /api responds with 200 containing "{"ok":true}"', () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
+      .get('/api/')
+      .expect(200, '{"ok":true}')
   })
 })
