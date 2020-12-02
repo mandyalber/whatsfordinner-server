db805583cc289375b241a7d496c2a8a5d66aeb8e
Mandy Alber <mandy.alber@gmail.com>
2020-12-02 15:00:52 -0800

    updated CORS header

--- src/app.js
+++ src/app.js
@@ -13,11 +13,7 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption))
 app.use(helmet())
 
-app.use(
-  cors({
-      origin: CLIENT_ORIGIN
-  })
-);
+app.use(cors());
 /*
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", 'https://whatsfordinner-app.vercel.app/');
