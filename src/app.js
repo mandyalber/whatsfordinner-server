ea67233eba1a8dc258d760856f8bd878ee5d40f9
Mandy Alber <mandy.alber@gmail.com>
2020-12-02 11:40:29 -0800

    moved store to server

--- src/app.js
+++ src/app.js
@@ -4,23 +4,20 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { CLIENT_ORIGIN, NODE_ENV } = require('./config')
-
+const recipeList = require('./store.json')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}))
+app.use(cors({ origin: CLIENT_ORIGIN }))
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
-});
+app.get('/api/recipes', function handleGetMovies(req, res) {
+  let response = recipeList
+   res.json(response)
+})
 
 app.use(function errorHandler(error, req, res, next) {
   let response
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://whatsfordinner-app.vercel.app/',
   }
