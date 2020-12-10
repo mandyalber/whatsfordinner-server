40538183f1fe851e6e5ec08707cf6251dfe72590
Mandy Alber <mandy.alber@gmail.com>
2020-12-10 11:56:11 -0800

    added endpoint for random weekday recipes

--- src/saved-recipes/saved-recipes-router.js
+++ src/saved-recipes/saved-recipes-router.js
@@ -5,24 +5,46 @@ const SavedRecipesService = require('./saved-recipes-service')
 const savedRecipesRouter = express.Router()
 const jsonParser = express.json()
 
+function getRandomArray(array) {
+    let newArray = [];
+
+    for (let i = 0; i < 7; i++) {
+        let idx = Math.floor(Math.random() * array.length)
+        newArray.push(array[idx])
+        array.splice(idx, 1)
+    }
+    return newArray
+}
+
 savedRecipesRouter
     .route('/')
     .get((req, res, next) => {
         const knexInstance = req.app.get('db')
         SavedRecipesService.getAllRecipes(knexInstance)
-        .then(recipes => {
-            res.json(recipes)
-        })
+            .then(recipes => {
+                res.json(recipes)
+            })
             .catch(next)
     })
     .post(jsonParser, (req, res, next) => {
         const { recipeId, title, sourceUrl, image, summary } = req.body
         const newRecipe = { recipeId, title, sourceUrl, image, summary }
-        
+
         SavedRecipesService.insertRecipe(req.app.get('db'), newRecipe)
             .then(recipe => {
-                res.status(201).json(recipe) 
+                res.status(201).json(recipe)
                 console.log(recipe)
+            })
+            .catch(next)
+    })
+
+savedRecipesRouter
+    .route('/weekdays')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        SavedRecipesService.getAllRecipes(knexInstance)
+            .then(recipes => {
+                res.json(getRandomArray(recipes))
             })
             .catch(next)
     })
