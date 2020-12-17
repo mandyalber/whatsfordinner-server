c9d64c806f9782c5fd111154b2ee1498b4fb5ebe
Mandy Alber <mandy.alber@gmail.com>
2020-12-17 11:13:56 -0800

    fix db connection

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://whatsfordinner-app.vercel.app/',
     API_ENDPOINT: process.env.API_ENDPOINT || 'https://api.spoonacular.com/recipes/complexSearch?',
     API_KEY: process.env.API_KEY,
-    DATABASE_URL: process.env.DB_URL || "postgres://rmeaeyrvrliqti:dc0d40fe384e34cdba94b7830027a3ac47cd6b3df2a84fa3b52404c754e0d4a0@ec2-3-218-123-191.compute-1.amazonaws.com:5432/d51ehljf4bhffj?ssl=true",
+    DATABASE_URL: process.env.DB_URL || "postgres://rmeaeyrvrliqti:dc0d40fe384e34cdba94b7830027a3ac47cd6b3df2a84fa3b52404c754e0d4a0@ec2-3-218-123-191.compute-1.amazonaws.com:5432/d51ehljf4bhffj&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
   }
