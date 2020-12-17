0576bd2a9ad32fd1383ffedf4324bcd460aeacfb
Mandy Alber <mandy.alber@gmail.com>
2020-12-17 11:46:55 -0800

    fix db connection

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://whatsfordinner-app.vercel.app/',
     API_ENDPOINT: process.env.API_ENDPOINT || 'https://api.spoonacular.com/recipes/complexSearch?',
     API_KEY: process.env.API_KEY,
-    DATABASE_URL: process.env.DATABASE_URL || "'postgresql://postgres:password@localhost/whatsfordinner",
+    DATABASE_URL: /*process.env.DATABASE_URL || */"postgresql://postgres:password@localhost/whatsfordinner",
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
   }
--- src/server.js
+++ src/server.js
@@ -5,9 +5,6 @@ const { PORT, DATABASE_URL } = require('./config')
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
 })
 app.set('db', db)
 
