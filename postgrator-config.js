7f59c7e47e478ba89a019f1c939233ee3dfa1432
Mandy Alber <mandy.alber@gmail.com>
2020-12-17 11:52:43 -0800

    fix db connection

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
