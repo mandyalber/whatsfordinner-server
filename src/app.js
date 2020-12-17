8f78e4a65db9eb924810d7ed39e0c5b91a1d6a9c
Mandy Alber <mandy.alber@gmail.com>
2020-12-17 14:37:57 -0800

    cors error

--- src/app.js
+++ src/app.js
@@ -18,6 +18,40 @@ app.use(helmet())
 
 app.use(cors());
 
+// {"authToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE2MDgyNDI4MDksInN1YiI6Im1lQG1lLmNvbSJ9.C_4FlDMtdiPF7nJzINNI72XV0Hj6fn1UZPVRUC3TW5o","name":"Mandy"}
+// https://infinite-bayou-90165.herokuapp.com/api/search-recipes?cuisine=african
+// curl --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE2MDgyNDI4MDksInN1YiI6Im1lQG1lLmNvbSJ9.C_4FlDMtdiPF7nJzINNI72XV0Hj6fn1UZPVRUC3TW5o" 
+// Request becomes
+// https://api.spoonacular.com/recipes/complexSearch?cuisine=African&apiKey=e912a913c4814563bb572613379df5e3
+/* Response
+<!DOCTYPE html>
+        <html>
+          <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta charset="utf-8">
+                <title>Application Error</title>
+                <style media="screen">
+                  html,body,iframe {
+                        margin: 0;
+                        padding: 0;
+                  }
+                  html,body {
+                        height: 100%;
+                        overflow: hidden;
+                  }
+                  iframe {
+                        width: 100%;
+                        height: 100%;
+                        border: 0;
+                  }
+                </style>
+          </head>
+          <body>
+                <iframe src="//www.herokucdn.com/error-pages/application-error.html"></iframe>
+          </body>
+        </html>
+*/
+
 app.use('/api/saved-recipes', savedRecipesRouter)
 app.use('/api/search-recipes', searchRecipesRouter)
 app.use('/api/auth', authRouter)
--- src/search-recipes/search-recipes-service.js
+++ src/search-recipes/search-recipes-service.js
@@ -29,10 +29,13 @@ const SearchRecipeService = {
     //console.log(url)
     const filters = [diet, intolerances, cuisine, includeIngredients, excludeIngredients]
 
+    console.log('Search URL', url);
+
     fetch(url)
         .then(response => {
+            console.log('Search response', response);
             if (!response.ok) {
-                throw new Error(response.status)
+              throw new Error(response.status)
             }
             return response.json()
         })
@@ -44,3 +47,40 @@ const SearchRecipeService = {
 }
 
 module.exports = SearchRecipeService
+
+/*
+
+2020-12-17T22:09:44.919417+00:00 heroku[router]: at=error code=H12 desc="Request timeout" method=GET path="/api/search-recipes?cuisine=african&diet=&intolerances=&includeIngredients=&excludeIngredients=" host=infinite-bayou-90165.herokuapp.com request_id=8dd968b4-2450-41c9-8f32-7a0604f92ad7 fwd="64.18.9.208" dyno=web.1 connect=1ms service=30000ms status=503 bytes=0 protocol=https
+
+2020-12-17T22:09:44.919857+00:00 app[web.1]: GET /api/search-recipes?cuisine=african&diet=&intolerances=&includeIngredients=&excludeIngredients= - - - - ms
+
+2020-12-17T22:18:12.517498+00:00 app[web.1]: 
+
+2020-12-17T22:18:12.518752+00:00 app[web.1]: GET /api/search-recipes?cuisine=african 401 32 - 1.099 ms
+
+2020-12-17T22:18:12.520996+00:00 heroku[router]: at=info method=GET path="/api/search-recipes?cuisine=african" host=infinite-bayou-90165.herokuapp.com request_id=1e28cb80-771f-4712-9754-7ee577409e19 fwd="64.18.9.197" dyno=web.1 connect=1ms service=3ms status=401 bytes=838 protocol=https
+
+2020-12-17T22:21:40.501589+00:00 app[web.1]: Error: 401
+
+2020-12-17T22:21:40.501607+00:00 app[web.1]:     at /app/src/search-recipes/search-recipes-service.js:35:23
+
+2020-12-17T22:21:40.501609+00:00 app[web.1]:     at processTicksAndRejections (internal/process/task_queues.js:97:5)
+
+2020-12-17T22:22:10.317651+00:00 app[web.1]: GET /api/search-recipes?cuisine=african - - - - ms
+
+2020-12-17T22:22:10.317838+00:00 heroku[router]: at=error 
+code=H12
+desc="Request timeout"
+method=GET
+path="/api/search-recipes?cuisine=african"
+host=infinite-bayou-90165.herokuapp.com
+request_id=05affed3-1c7f-4650-ac47-3a3e370f994a
+fwd="64.18.9.201"
+dyno=web.1
+connect=0ms
+service=30000ms
+status=503
+bytes=0
+protocol=https
+
+*/
