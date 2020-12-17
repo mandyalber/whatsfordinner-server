42a0625a664a74f166e89338aa54c5c097a94927
Mandy Alber <mandy.alber@gmail.com>
2020-12-17 12:28:50 -0800

    cors error

--- src/search-recipes/search-recipes-router.js
+++ src/search-recipes/search-recipes-router.js
@@ -1,10 +1,7 @@
 const express = require('express')
-const cors = require('cors')
 const { requireAuth } = require('../auth/jwt-auth')
 const searchRecipesRouter = express.Router()
 const SearchRecipeService = require('./search-recipes-service')
-
-app.use(cors());
 
 searchRecipesRouter
     .route('/')
