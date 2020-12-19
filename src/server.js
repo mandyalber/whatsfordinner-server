f283ddaff78caf1667cce809203c66a9eb75b151
Mandy Alber <mandy.alber@gmail.com>
2020-12-19 09:36:01 -0800

    db error

--- src/server.js
+++ src/server.js
@@ -4,14 +4,7 @@ const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : 'ec2-34-200-181-5.compute-1.amazonaws.com',
-    user: 'xzchewmhnclbtq',
-    database : 'ddao5iptqkfu82',
-    password : '0a2dd6e6f681a38ef49044068895da537a586d71d883c446cf678cef7d24a9d4',
-    port : '5432',
-    ssl: true
-  }
+  connection: DATABASE_URL,
 })
 app.set('db', db)
 
